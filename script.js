7a366b17b287583545100f66db2234a98934b95e
Abhishek Choudhari <abhishek007coc@gmail.com>
2022-12-21 13:50:59 +0530

    grid-commit5

--- script.js
+++ script.js
@@ -1,59 +1,64 @@
 // Creating 16x16 child divs for grid-container
 const gridContainer = document.querySelector('.grid-container');
-
 let gridVar = 16; // Default value for Grid
-for(let i = 0; i < gridVar; i++){
-     let tempDiv = document.createElement('div');
-     tempDiv.classList.add('itemDivParent' + i);
-     tempDiv.style.width = '100%';
-     tempDiv.style.height = '5.84vh';
-     tempDiv.style.border = '1px solid black';
-     tempDiv.style.display = 'flex';
-     tempDiv.style.flexDirection = 'row';
-     tempDiv.style.justifyContent = 'space-between';
-     tempDiv.style.alignItems = 'center';
-     
-     for(let j = 0; j < gridVar; j++){
-        //Creating Row of Divs
-        let tempDivChild = document.createElement('div');
-        tempDivChild.classList.add('itemDivChild' + j);
-        tempDivChild.style.width = '6.1vw';
-        tempDivChild.style.height = '5.84vh';
-        tempDivChild.style.border = '1px solid blue';
 
-        //Adding Hover Affect
-        tempDivChild.addEventListener('mouseover', (e) => {
-            tempDivChild.style.backgroundColor = 'black';
-            //return;
-        });
+//Function Definition to build Grid of Divs
+function createGridDivs(gridVar){
+   for(let i = 0; i < gridVar; i++){
+   let tempDiv = document.createElement('div');
+   tempDiv.classList.add('itemDivParent' + i);
+   tempDiv.style.width = '100%';
+   tempDiv.style.height = '5.84vh';
+   tempDiv.style.border = '1px solid black';
+   tempDiv.style.display = 'flex';
+   tempDiv.style.flexDirection = 'row';
+   tempDiv.style.justifyContent = 'space-between';
+   tempDiv.style.alignItems = 'center';
+   
+   for(let j = 0; j < gridVar; j++){
+      //Creating Row of Divs
+      let tempDivChild = document.createElement('div');
+      tempDivChild.classList.add('itemDivChild' + j);
+      tempDivChild.style.width = '6.1vw';
+      tempDivChild.style.height = '5.84vh';
+      tempDivChild.style.border = '1px solid blue';
 
-        //Removing Hover Affect
-        tempDivChild.addEventListener('mouseleave', (e) => {
-            tempDivChild.style.backgroundColor = 'white';
-            return;
-        });
-        tempDiv.appendChild(tempDivChild);
-     }
-     gridContainer.appendChild(tempDiv);
+      //Adding Hover Affect
+      tempDivChild.addEventListener('mouseover', (e) => {
+          tempDivChild.style.backgroundColor = 'black';
+          //return;
+      });
 
-     //Button1 takes Input from user and sets the Grid Variable
-     /*function handleButton1(){
-        gridVar = prompt('Enter the Number of boxes per side.');
-        console.log(gridVar);
-        
-     }*/
+      //Removing Hover Affect
+      tempDivChild.addEventListener('mouseleave', (e) => {
+          tempDivChild.style.backgroundColor = 'white';
+          return;
+      });
+      tempDiv.appendChild(tempDivChild);
+   }
+   gridContainer.appendChild(tempDiv);
 
-     
-     
+   
+
+   
+   
 }
+}
+//Function Call to Build grid of Divs
+createGridDivs(gridVar);
 
 
 //EventListener for Button1
 const gridButton1 = document.querySelector('#button1');
 gridButton1.addEventListener('click', (e) => {
    gridVar = prompt('Enter the Number of boxes per side.');
+   let oldDiv = document.querySelector('.grid-container');
+   while(oldDiv.firstChild){
+      oldDiv.removeChild(oldDiv.firstChild);
+}
+
    console.log(gridVar)
-   console.log('Button1 was clicked');
+   createGridDivs(gridVar);
    //location.reload();
    
    return;
