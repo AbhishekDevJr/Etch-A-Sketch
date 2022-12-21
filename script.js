15091cfe5d247a6d0cb2e52fd453dcf3364b38e4
Abhishek Choudhari <abhishek007coc@gmail.com>
2022-12-21 19:07:48 +0530

    grid-commit6

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Creating 16x16 child divs for grid-container
 const gridContainer = document.querySelector('.grid-container');
 let gridVar = 16; // Default value for Grid
-gridContainer.style.height = '91.6vh';
-gridContainer.style.width = '98vw';
+gridContainer.style.height = '60vh';
+gridContainer.style.width = '60vw';
 
 
 
@@ -10,8 +10,8 @@ gridContainer.style.width = '98vw';
 function createGridDivs(gridVar){
 
    //Grid Div height logic
-   let heightVarDiv = 100/gridVar;
-   console.log(heightVarDiv);
+   let heightVarDiv = 50/gridVar;
+   console.log('height of single div-->' + heightVarDiv);
    for(let i = 0; i < gridVar; i++){
    let tempDiv = document.createElement('div');
    tempDiv.classList.add('itemDivParent' + i);
@@ -71,7 +71,7 @@ gridButton1.addEventListener('click', (e) => {
       createGridDivs(gridVar);
    }
    else{
-      alert('Please enter a value less than 100.')
+      alert('Please enter a value less than 100 or a valid number.')
    }
 
    
